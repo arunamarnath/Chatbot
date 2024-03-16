6675e701a0f1495b29c6ea1bf296558efc151526
bharathnarayanan22 <bharathnarayanan.pa2022cse@sece.ac.in>
2024-03-16 16:07:15 +0530

    Day 2 frontend

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useEffect } from "react";
 import { useState } from "react";
+import { loginUser } from "../helpers/api-communicator";
 
 type User = {
     name: string;
@@ -23,7 +24,13 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
     }, []);
 
-    const login = async (email: string, password: string) => { };
+    const login = async (email: string, password: string) => { 
+        const data = await loginUser(email, password)
+        if(data) {
+            setUser({ email: data.email, name: data.name })
+            setIsLoggedIn(true)
+        }
+    };
     const signup = async (name: string, email: string, password: string) => { };
     const logout = async () => { };
 
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Chat=()=>{
     return(
-        <>
-        </>
+        <div>
+            chat
+        </div>
     )
 }
 
